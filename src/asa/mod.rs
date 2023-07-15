95dc55af325999903f1ea21aefb01f0dba095e80
Serge Barinov <bool.rus@gmail.com>
2023-07-15 14:28:43 +0300

    iam auth: second steps

--- src/asa/mod.rs
+++ src/asa/mod.rs
@@ -1,10 +1,15 @@
 
 
-use std::{collections::HashMap, time::Instant};
+use std::sync::{Arc, RwLock};
+use std::time::{UNIX_EPOCH, SystemTime};
 
-use jwt_simple::{prelude::{Claims, PS256KeyPair, RSAKeyPairLike, Clock, PS256PublicKey, RSAPublicKeyLike, HS256Key, RS256KeyPair, NoCustomClaims}, token::Token};
+use jwt_simple::token::Token;
+use jwt_simple::prelude::{Claims, PS256KeyPair, RSAKeyPairLike, PS256PublicKey, RSAPublicKeyLike, NoCustomClaims};
 use serde::{Serialize, Deserialize};
-use yandex_cloud::yandex::cloud::iam::v1::{CreateIamTokenForServiceAccountRequest, CreateIamTokenRequest, create_iam_token_request::Identity};
+use yandex_cloud::yandex::cloud::iam::v1::CreateIamTokenResponse;
+
+use crate::auth::Credentials;
+
 
 
 #[test]
@@ -27,34 +32,83 @@ pub struct ServiceAccountKey {
 fn test_de() {
     use std::fs::File;
     use std::io::BufReader;
-    let path = "test-env/authorized_key.json";
+    let path = "test-env/authorized_key_4096.json";
     let file = File::open(path).unwrap();
 
     let reader = BufReader::new(file);
     let key: ServiceAccountKey = serde_json::from_reader(reader).unwrap();
     println!("public key: \n{}",key.public_key);
     println!("private key: \n{}", key.private_key);
-    let token = make_token(&key);
+    let token = make_jwt(&key);
 
     println!("token: {token}");
 
 }
-
-pub struct ServiceAccountCredential {
-    key: String,
+#[derive(Clone)]
+pub struct ServiceAccountCredentials {
+    token: Arc<RwLock<String>>,
 }
 
-fn make_token(key: &ServiceAccountKey) -> String {
+impl Credentials for ServiceAccountCredentials {
+    fn token(&self) -> crate::AsciiValue {
+        self.token.read().unwrap().clone().try_into().unwrap()
+    }
+}
+
+impl ServiceAccountCredentials {
+    pub async fn create(key: ServiceAccountKey) -> Result<Self, tonic::Status> {
+        let mut response = request_iam_token(&key).await?;
+        let token = Arc::new(RwLock::new(response.iam_token.clone()));
+        let result = Self {token};
+        let token = Arc::downgrade(&result.token);
+        tokio::spawn(async move {
+            loop {
+                let sleep_duration = invoke_sleep_duration(&response);
+                tokio::time::sleep(sleep_duration).await;
+                response = request_iam_token(&key).await.unwrap();
+                if let Some(token) = token.upgrade() {
+                    *token.write().unwrap() = response.iam_token.clone();
+                    log::info!("Iam token updated");
+                } else {
+                    break;
+                }
+            }
+        });
+        Ok(result)
+    }
+}
+
+fn invoke_sleep_duration(response: &CreateIamTokenResponse) -> tokio::time::Duration {
+    use std::time::Duration;
+    let CreateIamTokenResponse {iam_token: _, expires_at} = response;
+    let expires = if let Some(ts) = expires_at {
+        (UNIX_EPOCH + Duration::from_secs(ts.seconds as u64)) //convert ts to SystemTime
+            .duration_since(SystemTime::now() + Duration::from_secs(60)) //расчитываем время сна (с запасом)
+            .unwrap_or_default()
+    } else  {
+        Duration::from_secs(50)
+    };
+    expires.into()
+}
+
+pub async fn request_iam_token(key: &ServiceAccountKey) -> Result<CreateIamTokenResponse, tonic::Status> {
+    let jwt = make_jwt(key);
+    let url = "grpcs://iam.api.cloud.yandex.net:443";
+    let endpoint = crate::client::create_endpoint(url.try_into().unwrap());
+    let mut client = yandex_cloud::yandex::cloud::iam::v1::iam_token_service_client::IamTokenServiceClient::new(endpoint.connect_lazy());
+    let request = yandex_cloud::yandex::cloud::iam::v1::CreateIamTokenRequest {
+        identity: Some(yandex_cloud::yandex::cloud::iam::v1::create_iam_token_request::Identity::Jwt(jwt))
+    };
+    let resp = client.create(request).await?;
+    Ok(resp.into_inner())
+}
+
+fn make_jwt(key: &ServiceAccountKey) -> String {
     use jwt_simple::prelude::Duration;
-    //let mut headers = HashMap::new();
-    //headers.insert("kid".to_owned(), key.id.clone());
-    //let claims = Claims::with_custom_claims(headers, Duration::from_hours(1))
-    let mut claims = Claims::create(Duration::from_hours(1))
+    let mut claims = Claims::create(Duration::from_mins(1))
         .with_issuer(&key.service_account_id)
         .with_audience("https://iam.api.cloud.yandex.net/iam/v1/tokens");
 
-    //claims.issued_at = Some(now);
-    //claims.expires_at = Some(exp);
     let pair = PS256KeyPair::from_pem(&key.private_key).unwrap().with_key_id(&key.id);
     println!("padding scheme: {:?}", pair.padding_scheme());
     let token = pair.sign(claims).unwrap();
@@ -74,26 +128,53 @@ async fn update_token() {
 async fn get_token() {
     use std::fs::File;
     use std::io::BufReader;
-    let path = "test-env/authorized_key.json";
+    let path = "test-env/authorized_key_4096.json";
     let file = File::open(path).unwrap();
 
     let reader = BufReader::new(file);
     let key: ServiceAccountKey = serde_json::from_reader(reader).unwrap();
     println!("public key: \n{}",key.public_key);
-    let token = make_token(&key);
+    let token = make_jwt(&key);
     println!("token: {token}");
     use crate::client;
     let url = "grpcs://iam.api.cloud.yandex.net:443";
     let endpoint = client::create_endpoint(url.try_into().unwrap());
     let channel = endpoint.connect_lazy();
     let mut client = yandex_cloud::yandex::cloud::iam::v1::iam_token_service_client::IamTokenServiceClient::new(channel);
-    let request = CreateIamTokenRequest {
-        identity: Some(Identity::Jwt(token))
+    let request = yandex_cloud::yandex::cloud::iam::v1::CreateIamTokenRequest {
+        identity: Some(yandex_cloud::yandex::cloud::iam::v1::create_iam_token_request::Identity::Jwt(token))
     };
     let resp = client.create(request).await.unwrap();
     let response = resp.into_inner();
     println!("response: {response:?}");
-    println!("expires: {} seconds", (response.expires_at.unwrap().seconds as u64 - 1689335892));
+    let expires = response.expires_at.as_ref().unwrap();
+    let expires = UNIX_EPOCH + std::time::Duration::from_secs(expires.seconds as u64);
+    let after = expires.duration_since(SystemTime::now()).unwrap();
+    println!("expires: {}",after.as_secs());
+
+
+    use std::env::var;
+    let url = var("YDB_URL").expect("YDB_URL not set");
+    let db_name = var("DB_NAME").expect("DB_NAME not set");
+
+    let endpoint = client::create_endpoint(url.try_into().unwrap());
+    let channel = endpoint.connect_lazy();
+    let mut service = crate::YdbConnection::new(channel, db_name.as_str().try_into().unwrap(), response.iam_token);
+    
+         // how to use it, e.g. use discovery service:
+    use crate::generated::ydb::discovery::ListEndpointsRequest;
+    let endpoints_response = service.discovery().list_endpoints(
+        ListEndpointsRequest{
+            database: db_name.into(), 
+            ..Default::default()
+        }
+    ).await.unwrap();
+    
+    // how you can parse response to invoke result with YdbResponseWithResult trait
+    use crate::YdbResponseWithResult;
+    let endpoints_result = endpoints_response.get_ref().result().unwrap();
+    println!("endpoints {:?}", endpoints_result.endpoints);
+    assert!(endpoints_result.endpoints.len() > 0);
     
 }
 
