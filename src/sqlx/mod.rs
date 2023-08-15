87b429e2396da42806f76dca712d8192c8b4277d
Bool <bool.rus@gmail.com>
2023-08-15 13:54:15 +0300

    mod rename

--- src/sqlx/mod.rs
+++ src/sqlx/mod.rs
@@ -1,14 +1,15 @@
 mod dumb;
-mod value;
+mod entities;
 mod database;
 mod error;
 mod connection;
 mod executor;
 mod types;
+//TODO: спрятать под фичу
 mod minikql;
 
 type YdbConnection = crate::YdbConnection<crate::auth::UpdatableToken>;
 //pub use query::*;
 pub use database::*;
-pub use value::*;
+pub use entities::*;
 pub use super::error::YdbError;
